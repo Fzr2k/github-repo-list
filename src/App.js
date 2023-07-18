77ffec53ce79eec4702bf4e6c9388dffd386cce5
Faraz Ahmed <faraz.ahmed@arkhitech.com>
2023-07-18 22:33:18 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 import './App.css';
 
 function App() {
+  const [githubUsername, setGithubUsername] = useState('');
+  const [githubRepositories, setGithubRepositories] = useState([]);
+  const [showGithubRepositories, setShowGithubRepositories] = useState(false);
+
+  const getRepositoryList = async () => {
+    fetch(`https://api.github.com/users/${githubUsername}/repos`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Incorrect Github username. Please re-check');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setGithubRepositories(data);
+        setShowGithubRepositories(true);
+      })
+      .catch((error) => {
+        console.error(error);
+        setGithubRepositories([]);
+        setShowGithubRepositories(false);
+        Swal.fire('Error', error.message, 'error');
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>GitHub Repository Fetcher</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={githubUsername}
+          onChange={(e) => setGithubUsername(e.target.value)}
+        />
+        <button onClick={getRepositoryList}>Fetch Repositories</button>
+      </div>
+      {showGithubRepositories && (
+        <>
+          <ul>
+            {githubRepositories.map((repo) => (
+              <li key={repo.id}>{repo.name}</li>
+            ))}
+          </ul>
+        </>
+        )}
     </div>
   );
 }
